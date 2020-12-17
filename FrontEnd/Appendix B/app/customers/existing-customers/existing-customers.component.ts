b21f8dcb767e2b2499d6b2bf5fc439af387a5ad2
nicolapv <nicola.p.v.a@gmail.com>
2020-12-17 19:27:54 +0100

    Add files via upload

---
+++ FrontEnd/Appendix B/app/customers/existing-customers/existing-customers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-existing-customers',
+  templateUrl: './existing-customers.component.html',
+  styleUrls: ['./existing-customers.component.css']
+})
+export class ExistingCustomersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
